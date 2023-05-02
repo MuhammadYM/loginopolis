574870e0619d06bb39f6e876edbda800f19c821c
Muhammad <muhammadalnabhani@gmail.com>
2023-05-02 18:12:06 +0100

    tests all pass

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { User } = require('./db');
+const { User } = require("./db");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   try {
-    res.send('<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>');
+    res.send(
+      "<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>"
+    );
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
-// POST /register
-// TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = new User(req.body);
+    let hashedPassword = await bcrypt.hash(password, 9);
+    const user = await User.create({ username, password: hashedPassword });
+    res.send(`successfully created user ${username}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+      res.send("User doesn't exist");
+    }
+
+    const matcher = await bcrypt.compare(password, user.password);
+    // console.log(password);
+    // console.log(hashedPassword);
+    if (matcher) {
+      res.send(`successfully logged in user ${username}`);
+    } else {
+      res.send(`incorrect username or password`);
+    }
+  } catch (error) {
+    res.status(200).json();
+    console.log(error);
+  }
+});
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
